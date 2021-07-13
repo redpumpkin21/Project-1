a9b02aab718356a298c9897bf857de21733aa81c
Christopher Johnson <christopherjohnson@Christophers-MacBook-Pro.local>
2021-07-13 01:15:13 -0400

    navbar and scrolling to done

--- js/app.js
+++ js/app.js
@@ -3,3 +3,49 @@
 if (screen.width >= 1024){
     console.log('here')
 }
+
+$('button.social').click((pop) => {
+    alert('YA CLICKED ME BITCH')
+})
+
+$('#about').click((go) => {
+    alert('fix me and scroll snap me so I can get my satisfaction')
+})
+
+// $('#contact').ready((goagain) => {
+//     contact.querySelector('.contactme').scrollIntoView({
+//         behavior: 'smooth' 
+//       });
+// })
+
+
+$('#resume').click((pdf) => {
+    window.open('https://drive.google.com/file/d/1ziX7qCm-XjDmpVvqmUpA5yxT51UzaoEP/view?ths=true','mywin','width=500,height=500')
+
+    })
+
+//     $(document).ready(function() {
+//         $("#contact").click(function() {
+//           $("html").animate(
+//             {
+//               scrollTop: $(".contactme").offset().top
+//             })
+//             800 //speed
+//     })   
+// })
+
+// $(document).ready((goagain) => {
+//      $('#contact').click((yay) => {
+//          $('html, body').animate({
+//              scrollTop:$('.contactme').offset().top         
+//          }, 800);
+//      })
+// })
+
+// $("#contact").click((go) => {
+//     alert($('contactme').scrollTop())
+// })
+    
+// $('html, body').animate({
+//     scrollTop: $('.contactme').offset().top
+//   }, 800, function(){
